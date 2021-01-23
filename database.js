a3a89d6151952b4a7f580b9e6532723ecc19cd18
Javier Martin Gomez <javichu13martin@gmail.com>
2021-01-23 18:08:41 +0100

    comentario sergio

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const { mongodb } = require('./keys');
 
 mongoose.set('useFindAndModify', false);
 mongoose.connect('mongodb+srv://Jaserlu:1234@cluster0.vv9pw.mongodb.net/test?retryWrites=true&w=majority',{
-    
+    /*Comentario*/
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: true,
