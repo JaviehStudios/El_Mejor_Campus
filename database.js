ae0b60b04424e05b0ec9652d56338094e2375a68
Javier Martin Gomez <javichu13martin@gmail.com>
2021-01-23 17:57:09 +0100

    database update

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { mongodb } = require('./keys');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://JavierMartin:1234@cluster0.vv9pw.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://Jaserlu:1234@cluster0.vv9pw.mongodb.net/test?retryWrites=true&w=majority',{
     
     useCreateIndex: true,
     useNewUrlParser: true,
